12d23d473f0285415f92476c56d8b00d7d9b2a62
Braden <bradent@outlook.com>
2022-11-03 19:48:42 -0600

    Installs Blazorise Blazor component packages.

--- CPSC481_Prototype/Program.cs
+++ CPSC481_Prototype/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using CPSC481_Prototype;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +10,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorise(o => o.Immediate = true).AddBootstrapProviders().AddFontAwesomeIcons();
 
 await builder.Build().RunAsync();

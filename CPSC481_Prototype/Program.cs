4adce2e576c66a605cc343bfd26be2e24710d08e
PKDT-93 <75346912+PKDT-93@users.noreply.github.com>
2022-11-09 16:33:50 -0700

    search prototype

--- CPSC481_Prototype/Program.cs
+++ CPSC481_Prototype/Program.cs
@@ -12,4 +12,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazorise(o => o.Immediate = true).AddBootstrapProviders().AddFontAwesomeIcons();
 
+
 await builder.Build().RunAsync();

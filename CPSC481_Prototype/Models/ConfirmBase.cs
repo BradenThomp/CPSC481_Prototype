78f7e8de057da6d8c19072c825f4bed66d0ef3d5
Quanzhi Zhang <quanzhi.zhang1@ucalgary.ca>
2022-12-09 23:24:40 -0700

    Confirmation popup window for remove item from wishlist and on hold in iteminfo page(need to use another way for the same window in on hold and wishlist page)

---
+++ CPSC481_Prototype/Models/ConfirmBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace PragimTech.Components
+{
+    public class ConfirmBase : ComponentBase
+    {
+        protected bool ShowConfirmation { get; set; }
+
+        [Parameter]
+        public string ConfirmationTitle { get; set; } = "Confirm Remove";
+
+        [Parameter]
+        public string ConfirmationMessage { get; set; } = "Are you sure you want to delete";
+
+        public void Show()
+        {
+            ShowConfirmation = true;
+            StateHasChanged();
+        }
+
+        [Parameter]
+        public EventCallback<bool> ConfirmationChanged { get; set; }
+
+        protected async Task OnConfirmationChange(bool value)
+        {
+            ShowConfirmation = false;
+            await ConfirmationChanged.InvokeAsync(value);
+        }
+    }
+}
